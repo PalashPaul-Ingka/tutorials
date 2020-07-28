0638968975024f56187bddd6c7d603d535bb007d
Palash Paul <palash.paul@ingka.com>
2020-07-28 11:36:35 +0200

    Update products.py

--- kubernetes/api-load-test/docker-image/locust-tasks/products.py
+++ kubernetes/api-load-test/docker-image/locust-tasks/products.py
@@ -3,6 +3,6 @@ import json
 
 def get_products():
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket('ingka-rrm-ugc-data')
-    input_blob = bucket.blob('testing/ugc_api.json').download_as_string()
+    bucket = storage_client.get_bucket('<bucket_name>')
+    input_blob = bucket.blob('<path_of_test_date_input_file>').download_as_string()
     return json.loads(input_blob)
